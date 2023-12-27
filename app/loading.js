b29cfce705a77bb495d272a8613c0dcf1292778b
mabgcm <bugucam@gmail.com>
2023-12-27 16:08:46 -0500

    up

--- app/loading.js
+++
@@ -1,6 +0,0 @@
-import Spinner from "app/components/Spinner/Spinner"
-
-export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <Spinner />
-}
