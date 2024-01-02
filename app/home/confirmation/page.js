f70393e3b6b0b9d8e92b236d44f2ea1258f95510
mabgcm <bugucam@gmail.com>
2024-01-02 06:47:37 -0500

    after payment confirmation page

--- app/home/confirmation/page.js
+++ app/home/confirmation/page.js
@@ -3,7 +3,19 @@ import { FaRegCircleCheck } from "react-icons/fa6";
 const page = () => {
     return (
         <div>
-            <div className="check-icon"><FaRegCircleCheck />
+            <div className="conf-text1 text-center mx-4">
+                <h4>You have secured your booking!</h4>
+            </div>
+
+            <div className="check-icon centered">
+                <FaRegCircleCheck
+                    color="green"
+                    size='250px' />
+            </div>
+
+            <div className="conf-text2 text-center mx-4">
+
+                <p className="fs-5">Thank you for selecting Neat Guys for your cleaning service. Our team will reach out to you shortly to coordinate the details before the scheduled service delivery.</p>
             </div>
         </div>
     )
