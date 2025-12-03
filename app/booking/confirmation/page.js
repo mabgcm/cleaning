ae483aea67aaa2fca8c424f95ef3b60c76cb6073
mabgcm <bugucam@gmail.com>
2025-12-03 12:29:40 -0500

    env updates

--- app/booking/confirmation/page.js
+++ app/booking/confirmation/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const confirmationPage = () => {
     return (
         <div>
-            <h4>Thanks for your booking with Neat Guys</h4>
+            <h4>Thanks for your booking with Brinova Cleaning</h4>
             <h5>Our team will contact you in 24 hours before the cleaning time.</h5>
         </div>
     )
