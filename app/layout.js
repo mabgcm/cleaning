4b6891de2b31e5cb1e7209807fe48ab7169694f1
mabgcm <bugucam@gmail.com>
2026-03-23 23:05:56 -0400

    pixel code

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import NavBar2 from './components/Navbar2';
 import { Analytics } from '@vercel/analytics/react';
 import FloatingSmsButton from './components/FloatingSmsButton';
+import Script from 'next/script';
 
 
 
@@ -19,6 +20,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '897162740140133');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=897162740140133&ev=PageView&noscript=1"/>`}
+        </noscript>
+      </head>
       <body className={inter.className}>
         <NavBar2 />
         {children}
